366c2a2c8ff08db3dd5ccacd21e9e605e8e49656
Hunter Owens <85654096+HunterO50@users.noreply.github.com>
2023-09-01 09:14:12 -0400

    Taco soup completeed

--- main.js
+++ main.js
@@ -5,21 +5,144 @@ for (let i = 0; i< harryPotterTitles.length; i++) {
 }
 */ 
 
-//Student Grades
-var grades = [92, 91, 75, 66, 52, 90, 83, 85, 64, 90, 72, 88, 77, 98, 100, 73, 92]
-for (i=0; i <= grades.length; i++) {
-if (grades[i] >= 0 && grades[i] <= 69) {
-    console.log("You got an F")} 
-    
-    else if (grades[i] >= 70 && grades[i] <= 76) {
-        console.log("You got an D")}
-        
-        else if (grades[i] >= 77 && grades[i] <= 84) {
-            console.log("You got an C")}
-
-            else if (grades[i] >= 85 && grades[i] <= 92) {
-                console.log("You got an B")}
-
-                else if (grades[i] >= 93 && grades[i] <= 100) {
-                    console.log("You got an A")}
+//Student Grades 
+/*
+let aGrades = {
+    score: 0,
+    actualGrade: "A"
 }
+let bGrades = {
+    score: 0,
+    actualGrade: "B"
+}
+let cGrades = {
+    score: 0,
+    actualGrade: "C"
+}
+let dGrades = {
+    score: 0,
+    actualGrade: "D"
+}
+let fGrades = {
+    score: 0,
+    actualGrade: "F"
+}
+
+let percentageGrades = 0
+
+var grades = [92, 91, 75, 66, 52, 90, 83, 85, 64, 90, 72, 88, 77, 98, 100, 73, 92]
+
+for(let taco = 0; taco < grades.length; taco++){
+
+if(grades[taco] <= 69){
+console.log("You got an F")
+fGrades.score++
+} else if(grades[taco] >= 70 && grades[taco] <= 76){
+    console.log("You got a D")
+    dGrades.score++
+} else if(grades[taco] >= 77 && grades[taco] <= 84) {
+    console.log("You got a C")
+    cGrades.score++
+} else if (grades[taco] >= 85 && grades[taco] <= 92){
+    console.log("You got a B")
+    bGrades.score++
+} else if(grades[taco] >= 93 && grades[taco] <= 100){  
+    console.log("You got an A")
+    aGrades.score++
+}
+}
+
+let letterGrades = [aGrades, bGrades, cGrades, dGrades, fGrades]
+let highScore = {
+score: 0,
+actualGrade: ""
+}
+for(let i = 0; i < letterGrades.length; i++){
+    if(letterGrades[i].score > highScore.score)
+    {
+        highScore = letterGrades[i]
+    }
+}
+console.log(highScore)
+console.log(aGrades, "This is A grades")
+console.log(bGrades, "This is B Grades")
+*/
+//Two four six eight 
+/*
+let s = ""
+for( let i = 2; i <= 8; i += 2 ){
+
+    s += i + " ";
+}
+console.log(s + "Who do we appreciate?!") 
+
+*/
+//Interrupting cow
+
+let sentenceArray = ["the", "cow", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+let sentence = ' '; 
+for (let i = 0; i < sentenceArray.length; i++) {
+    sentence += sentenceArray[i];
+    if((i + 1) % 3 === 0 && i !== sentenceArray.length - 1) {
+        sentence += " MOOOO!" ; 
+    } else if ( i !== sentenceArray.length - 1) {
+        sentence += " "
+    }
+}
+console.log(sentence) 
+    
+
+
+
+
+
+/*
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+//${name} was in the Beatles from ${year} to ${year}. He was born in ${year}. He contributed heavily to the ${albumName} Album.
+
+//Please note that these values are not correct. They're just placeholders that you will need to correctly fill out. 
+let string = ''
+for( let i = 0; i < beatles.members.length; i++ ) {
+    string += `${beatles.members[i].name} was in the Beatles from ${beatles.history.formed} to ${beatles.history.disbanded}. He was born in ${beatles.members[i].birth}. He contributed heavily to the ${beatles.albums[i]} Album.`;
+
+  
+if(beatles.members[i].death){
+    string += ` He died in ${beatles.members[i].death}. `
+} else{
+    string += " He is still alive"
+}}
+console.log(string)
+
+
+*/
+
+
+
